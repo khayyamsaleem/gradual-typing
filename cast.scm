5b1e0750ab12a42c96d8dbd43ed9ec801d853c57
Ramana Nagasamudram <rnagasam@stevens.edu>
2018-04-22 18:37:48 -0400

    Casting to an intermediate language

---
+++ cast.scm
@@ -0,0 +1,179 @@
+#|
+	Intermediate Typed Language
+|#
+
+(load "pmatch.scm")
+(load "types.scm")
+
+(define-syntax fn
+  ;;  curried
+  (syntax-rules (:)
+    ((_ (: v type) body ...)
+     (lambda (v) body ...))
+    ((_ ((: v type) v2 ...) body ...)
+     (lambda (v) (fn (v2 ...) body ...)))
+    ((_ () body ...)
+     (lambda () body ...))))
+
+(define (cast-symbol e type)
+  `(: ,e ,type))
+
+(define (cast? expr)
+  (and (pair? expr) (eq? (car expr) :)))
+
+(define (type-of-cast expr)
+  (caddr expr))
+
+(define (expr-of-cast expr)
+  (cadr expr))
+
+(define (cast-expr expr te)
+  (pmatch
+   expr
+   (,e (guard (symbol? e))
+       (let ((typed? (te/lookup te e)))
+	 (if typed?
+	     (cast-symbol e typed?)
+	     (cast-symbol e 'any))))
+   ((lambda (,v) ,body)
+    (let* ((v-cast (cast-expr v te))
+	   (te2 (te/extend te v (type-of-cast v-cast))))
+      `(fn ,v-cast ,(cast-expr body te2))))
+   ((fn (: ,v ,type) ,body)
+    `(fn (: ,v ,type) ,(cast-expr body (te/extend te v type))))
+
+   ;; ((,func)
+   ;;  (let ((func-type (caddr (cast-expr func te))))
+   ;;    (cast-symbol func (co-domain func-type))))
+   
+   ((,rator ,rand)
+    ;; rator type doesn't get cast properly
+    (let* ((rator-type (te/lookup te rator))
+	   (rand-cast (cast-expr rand te))
+	   (rand-type (type-of-cast rand-cast)))
+      (if (not rator-type)
+	  `(: (,(cast-symbol rator (make-arrow-type rand-type 'any)) ,rand-cast) any)
+	  (if (equal? rand-type (domain rator-type))
+	      `(: (,(cast-symbol rator rator-type) ,rand-cast) ,(co-domain rator-type)) 
+	      (if (~ (domain rator-type) rand-type)
+		  `(: (,(cast-symbol rator rator-type)
+		       (,(cast-symbol rand (domain rator-type)))) ,(co-domain rator-type))
+		  'cast-error)))))
+   
+   ((,rator . ,rands) (guard (predefined? rator))
+    (error "CAST -- PREDEFINED -- Not implemented yet"))
+
+   ((,rator . ,rands)
+    (error "CAST -- N-ARY APP -- Not implemented yet"))
+   ;; See if you can avoid all these clauses
+
+   (,e (guard (number? e)) (cast-symbol e 'number))
+
+   (,e (guard (string? e)) (cast-symbol e 'string))
+
+   (,e (guard (char? e)) (cast-symbol e 'char))
+
+   (,e (guard (boolean? e) (cast-symbol e 'boolean)))
+
+   (else (cast-symbol expr (car (dispatch-tag-contents (dispatch-tag expr)))))))
+
+;; Scrap
+(define (cast-tbl expr te)
+  (pmatch
+   expr
+   (,e (guard (symbol? e))
+       (let ((val (te/lookup te expr)))
+	 (if val
+	     te
+	     (te/extend te e 'any))))
+   ((fn (: ,v ,type) ,body)
+    (cast-tbl body (te/extend te v type)))
+   ((,rator ,rand)
+    (let* ((rator-type (te/lookup te rator))
+   	   (rand-cast (cast-tbl rand te))
+   	   (rand-type (te/lookup rand-cast rand)))
+      (if (not rator-type)
+   	  (te/extend rand-cast rator (make-arrow-type rand-type 'any))
+   	  (begin (display rator) (display " in te") (newline)
+   		 (if (equal? rand-type (domain rator-type))
+   		     (te/extend rand (domain rantor-type))
+   		     (if (~ (domain rator-type) rand-type)
+   			 (te/extend rand rand-type)
+   			 'cast-error))))))))
+
+(define (casto expr te)
+  (pmatch
+   expr
+   (,e (guard (symbol? e))
+       (let ((typed? (te/lookup te e)))
+	 (if typed?
+	     `(: ,e ,typed?)
+	     `(: ,e any))))
+   ((fn (: ,v ,type) ,body)
+    ;; Faced an error here with te/extend. te/extend should be adding everything to env.
+    `(fn (: ,v ,type) ,(casto body (cons (cons v type) te))))
+
+   ((,rator ,rand)
+    (let* ((rator-cast (begin (display "casting rator") (newline)
+			      (display (casto rator te)) (newline)
+			      (casto rator te)))
+	   (rator-type (type-of-cast rator-cast))
+	   (rand-cast (casto rand te))
+	   (rand-type (type-of-cast rand-cast)))
+      (if (any-type? rator-type)
+	  ;; function type is ?
+	  ;; `(: ((: ,rator ,(make-arrow-type rand-type 'any)) ,rand-cast) any)
+	  (if (pair? rator)
+	      `(: (,rator-cast ,rand-cast) any)
+	      `(: ((: ,rator ,(make-arrow-type rand-type 'any)) ,rand-cast) any))
+	  (if (equal? rand-type (domain rator-type))
+	      (if (pair? rator)
+		  `(: (,rator-cast ,rand-cast) ,(co-domain rator-type))
+		  `(: ((: ,rator ,rator-type) ,rand-cast) ,(co-domain rator-type)))
+	      (if (~ (domain rator-type) rand-type)
+		  (if (pair? rator)
+		      `(: (,rator-cast (: ,rand ,(domain rator-type))) ,(co-domain rator-type))
+		      `(: ((: ,rator ,rator-type) (: ,rand ,(domain rator-type))) ,(co-domain rator-type)))
+		  (error "CastError -- " expr))))))
+   
+   (,e (guard (number? e)) `(: ,e number))
+   (,e (guard (symbol? e)) `(: ,e symbol))
+   (,e (guard (char? e)) `(: ,e char))
+   (,e (guard (boolean? e)) `(: ,e boolean))
+   (else `(: ,expr ,(car (dispatch-tag-contents (dispatch-tag expr)))))))
+
+(define call/cc call-with-current-continuation)
+
+(define (castu expr te)
+  (define (make-cast expr type)
+    `(: ,expr ,type))
+  (define (un-cast cast)
+    (and (eq? (car cast) ':) (cadr cast)))
+  (pmatch
+   expr
+   (,e (guard (symbol? e))
+       (let ((typed? (te/lookup te e)))
+	 (if typed? (make-cast e typed?) (make-cast e 'any))))
+   ((fn (: ,v ,type) ,body)
+    (let* ((body-cast (castu body (cons (cons v type) te)))
+	   (co-domain-type (type-of-cast body-cast)))
+      (make-cast `(fn (: ,v ,type) ,body-cast) (make-arrow-type type co-domain-type))))
+   ((,rator ,rand)
+    (let* ((rator-cast (castu rator te))
+	   (rator-type (type-of-cast rator-cast))
+	   (rand-cast (castu rand te))
+	   (rand-type (type-of-cast rand-cast)))
+      (if (any-type? rator-type)
+	  (make-cast (list (make-cast (un-cast rator-cast) (make-arrow-type rand-type 'any))
+			   rand-cast) 'any)
+	  (if (equal? (domain rator-type) rand-type)
+	      (make-cast (list rator-cast rand-cast) (co-domain rator-type))
+	      (if (~ (domain rator-type) rand-type)
+		  (make-cast (list rator-cast (make-cast (un-cast rand-cast) (domain rator-type)))
+			     (co-domain rator-type))
+		  (error "CastError --" expr))))))
+   (,e (make-cast e (car (dispatch-tag-contents (dispatch-tag e)))))
+   ;; (,e (guard (number? e)) (make-cast e 'number))
+   ;; (,e (guard (string? e)) (make-cast e 'string))
+   ;; (,e (guard (boolean? e)) (make-cast e 'boolean))
+   (else (error "Not implemented"))))
---
+++ types.scm
@@ -0,0 +1,113 @@
+(load "pmatch.scm")
+
+#|
+	Types
+|#
+
+(define (any-type? type)
+  (eq? type 'any))
+
+(define (make-arrow-type s t)
+  `(-> ,s ,t))
+
+(define (make-arrow-type-arity s t arity)
+  (if (and (number? arity) (<= arity 1))
+      (make-arrow-type s t)
+      `(,(symbol-append '-> arity) ,s ,t)))
+
+(define (arrow-type? type)
+  (and (pair? type) (substring? "->" (symbol->string (car type)))))
+
+(define (domain arrow-type)
+  (if (any-type? arrow-type)
+      'any
+      (cadr arrow-type)))
+
+(define (co-domain arrow-type)
+  (if (any-type? arrow-type)
+      'any
+      (caddr arrow-type)))
+
+(define (arity arrow-type)
+  (if (arrow-type? arrow-type)
+      (if (> (string-length (symbol->string (car arrow-type))) 2)
+	  (let ((ari (string-ref (symbol->string (car arrow-type)) 2)))
+	    (if (eq? ari #\n)
+		'n
+		(char->digit ari)))
+	  1)
+      (error "ARITY -- input is not a function type, " arrow-type)))
+
+(define (pair-type? type)
+  (and (pair? type) (eq? (car type) '*)))
+
+(define (pair-arity type)
+  (and (pair-type? type) (length (cdr type))))
+
+(define (make-pair-type p1 p2 . rest)
+  `(* ,p1 ,p2 ,@rest))
+
+(define (list->pair-type ls)
+  `(* ,@ls))
+
+(define (nary->pair arrow-type)
+  (define (repeat symbol times)
+    (if (zero? times) '() (cons symbol (repeat symbol (- times 1)))))
+  (if (arrow-type? arrow-type)
+      (let ((ar (arity arrow-type)))
+	(if (or (eq? ar 'n) (= 1 ar))
+	    arrow-type
+	    (make-arrow-type
+	     `(* ,@(repeat (domain arrow-type) ar))
+	     (co-domain arrow-type))))
+      arrow-type))
+
+(define (make-list-type base-type)
+  `(list ,base-type))
+
+(define (list-type? type)
+  (and (pair? type) (eq? 'list (car type))))
+
+(define predefined-types
+  '((+ . (->n number number))
+    (- . (->n number number))
+    (* . (->n number number))
+    (/ . (->n number number))
+    (< . (->n number boolean))
+    (> . (->n number boolean))
+    (= . (->n number boolean))
+    (>= . (->n number boolean))
+    (<= . (->n number boolean))
+    (eq? . (->2 any boolean))
+    (eqv? . (->2 any boolean))
+    (equal? . (->2 any boolean))))
+
+(define (predefined? type)
+  (assoc type predefined-types))
+
+(define (get-predefined-type symbol)
+  (let ((type (assoc symbol predefined-types)))
+    (if type
+	(cdr type)
+	'any)))
+
+(define (~ s t)
+  (cond ((or (any-type? s) (any-type? t)))
+	((equal? s t))
+	((and (pair? s) (pair? t))
+	 (and (~ (car s) (car t))
+	      (~ (cdr s) (cdr t))))
+	(else #f)))
+
+;; (define (te/extend te var type)
+;;   (if (not (te/lookup te var))
+;;       (cons (cons var type) te)
+;;       te))
+
+(define (te/extend te var type)
+  (cons (cons var type) te))
+
+(define (te/lookup te var)
+  (let ((type (assoc var te)))
+    (if type (cdr type) #f)))
+
