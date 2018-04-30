16922be0e363f5235c6fc41aca379807bbf3f01a
Ramana Nagasamudram <rnagasam@stevens.edu>
2018-04-30 02:36:27 -0400

    Syntactic extensions and a sample file.

---
+++ sample.scm
@@ -0,0 +1,20 @@
+(fn (: x number) x)
+
+(fn (: x number)
+    (if (> x 0)
+	#t
+	#f))
+
+(fn (: x number)
+    (listof (: number) x x (* x x)))
+
+(if (> x 0)
+    #t
+    "false")
+
+(fn (: x number)
+    (if (> x 0)
+	#t
+	#f))
+
+(lambda (x) x)
---
+++ synext.scm
@@ -0,0 +1,45 @@
+#|
+	Syntactic Extensions
+|#
+
+(define-syntax fn-erase
+  (syntax-rules (:)
+    ((_ (: v type))
+     '(v))
+    ((_ ((: v type) v2 ...))
+     `(v ,@(fn-erase v2 ...)))))
+
+(define-syntax fn
+  (syntax-rules (:)
+    ((_ (: v type) (: return) body ...)
+     (lambda (v) body ...))
+    ((_ (: v type) body ...)
+     (lambda (v) body ...))
+    ((_ ((: v type) v2 ...) (: return) body ...)
+     (fn ((: v type) v2 ...) body ...))
+    ((_ ((: v type) v2 ...) body ...)
+     (let ((env (the-environment)))
+       (eval `(lambda ,(fn-erase ((: v type) v2 ...)) body ...) env)))
+    ((_ () body ...)
+     (lambda () body ...))))
+
+(define-syntax listof
+  (syntax-rules (:)
+    ((_ (: type) . items)
+     (quote items))))
+
+
+#|
+	REPL specific
+|#
+
+(define-syntax >>
+  ;; Need to fix
+  (syntax-rules ()
+    ((_ fn . args)
+     (begin
+       (display '(fn . args)) (newline)
+       (apply fn (quote args))))))
+
+
+
